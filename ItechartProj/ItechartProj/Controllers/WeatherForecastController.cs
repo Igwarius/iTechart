7e0c14ba5d3823438ae06da43a096fe9d51b5cb1
Igwarius <zolotnik1998@mail.ru>
2020-02-11 15:29:16 +0300

    remake

--- ItechartProj/ItechartProj/Controllers/WeatherForecastController.cs
+++ ItechartProj/ItechartProj/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Project.Controllers
+namespace ItechartProj.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- ItechartProj/ItechartProj/WeatherForecast.cs
+++ ItechartProj/ItechartProj/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Project
+namespace ItechartProj
 {
     public class WeatherForecast
     {
